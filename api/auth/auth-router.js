4e0d24df2e19d3d3b434e9d980552c0443574512
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-15 12:50:18 -0500

    wip auth router is complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../users/users-model')
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
@@ -47,6 +48,27 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
+    if (bcrypt.compareSync(req.body.password, req.user.password)) {
+      const token = buildToken(req.user)
+      res.json({
+        message: `${req.user.username} is back!`,
+        token: token,
+      })
+    } else {
+      next({ status: 401, message:'Invalid credentials' })
+    }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    role_name: user.role_name,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '1d',
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
